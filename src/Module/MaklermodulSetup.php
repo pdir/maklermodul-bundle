ba199cf5858ac0548e22afc6af1270b307228442
Mathias Arzberger <develop@pdir.de>
2018-03-21 12:02:14 +0100

    fix sql error for index file field

--- src/Module/MaklermodulSetup.php
+++ src/Module/MaklermodulSetup.php
@@ -24,7 +24,7 @@ class MaklermodulSetup extends \BackendModule
     /**
      * Maklermodul version
      */
-    const VERSION = '2.0.1';
+    const VERSION = '1.1.1';
 
     /**
      * Extension mode
